3e84a13de5ab850857141927ed8a98f7a6c0de8e
Matthew Rapati <mattrapati@gmail.com>
2019-12-04 22:32:13 -0500

    Added react-test-renderer. Added snapshot tests for Cell component. Update Life component test.

---
+++ src/__tests__/Cell.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Cell from "../components/cell";
+import renderer from "react-test-renderer";
+
+test("Cell renders correctly when not alive", () => {
+  const component = renderer.create(
+    <Cell
+      alive={false}
+      hue={0}
+      cellKey={"0|0"}
+      x={0}
+      y={0}
+      handleCellClick={() => {}}
+    />
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("Cell renders correctly when alive", () => {
+  const component = renderer.create(
+    <Cell
+      alive={true}
+      hue={0}
+      cellKey={"0|0"}
+      x={0}
+      y={0}
+      handleCellClick={() => {}}
+    />
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- src/__tests__/Life.test.js
+++ src/__tests__/Life.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Life from '../Life';
+import React from "react";
+import ReactDOM from "react-dom";
+import Life from "../Life";
+import { SPEEDS, CELL_SIZES, GRID_SIZES } from "../lib/constants";
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Life size={60} cellSize={12} speed={250} paused={true} />, div);
-    ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <Life
+      gridSize={GRID_SIZES[4]}
+      cellSize={CELL_SIZES[9]}
+      speed={SPEEDS[7]}
+      paused={true}
+    />,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
 });
